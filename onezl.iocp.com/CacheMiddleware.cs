13e4625c6a8cf5c64450458b5408da180dfe5220
luoys2 <luoys2@lenovo.com>
2018-06-01 10:25:35 +0800

    update memcache

---
+++ onezl.iocp.com/CacheMiddleware.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace onezl.iocp.com
+{
+
+
+  public class CacheMiddleware<TKey, TValue>
+  {
+    //使用示例：
+    //internal class ProductContentCache
+    //{
+    //  private static CacheMiddleware<string, Dictionary<string, string>> _instance;
+    //  private static readonly object Lock = new object();
+    //  public static CacheMiddleware<string, Dictionary<string, string>> Instance
+    //  {
+    //    get
+    //    {
+    //      if (_instance != null)
+    //      { return _instance; }
+    //      lock (Lock)
+    //      {
+    //        if (_instance == null)
+    //        { _instance = new CacheMiddleware<string, Dictionary<string, string>>(100000); }
+    //      }
+    //      return _instance;
+    //    }
+    //  }
+    //}
+
+    //var result = ProductContentCache.Instance.GetOrAdd("keystring", () =>
+    //{
+
+    //  return "valuestring";
+    //});
+
+
+
+
+
+
+
+
+
+
+
+
+    private SimpleMemCache<TKey, TValue> PrivateModdle_instance;
+    private readonly object Lock = new object();
+    public CacheMiddleware(long capacity)
+    {
+
+
+      lock (Lock)
+      {
+        if (PrivateModdle_instance == null)
+        {
+          PrivateModdle_instance = new SimpleMemCache<TKey, TValue>(capacity);
+        }
+      }
+
+
+
+    }
+
+
+    public TValue GetOrAdd(TKey key, Func<TValue> valueGenerator)
+    {
+      try
+      {
+        return this.PrivateModdle_instance.GetOrAdd(key, valueGenerator);
+      }
+      catch (Exception ex)
+      {
+
+        LogHelper.WriteLog("CacheMiddleware.GetOrAdd exp" + ex.Message + ex.Source);
+        return Task.FromResult(valueGenerator()).Result;
+      }
+
+    }
+
+
+    /// <summary>
+    /// put = add or update
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    public void Put(TKey key, TValue value)
+    {
+      try
+      {
+        this.PrivateModdle_instance.Put(key, value);
+      }
+      catch (Exception ex)
+      {
+
+        LogHelper.WriteLog("CacheMiddleware.Put exp" + ex.Message + ex.Source);
+      }
+
+    }
+    public void Remove(TKey key)
+    {
+      try
+      {
+        this.PrivateModdle_instance.Remove(key);
+      }
+      catch (Exception ex)
+      {
+        LogHelper.WriteLog("CacheMiddleware.Remove exp" + ex.Message + ex.Source);
+      }
+
+    }
+
+
+
+
+
+  }
+}
---
+++ onezl.iocp.com/SimpleMemCache.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+namespace onezl.iocp.com
+{
+  public class SimpleMemCache<TKey, TValue>
+  {
+
+
+    private long Capacity = 100000;
+    private object lockobj = new object();
+    private MemoryCache _cache = new MemoryCache(new MemoryCacheOptions());
+
+
+
+    public SimpleMemCache(long capacity)
+    {
+      Capacity = capacity;
+
+    }
+
+    public TValue GetOrAdd(TKey key, Func<TValue> valueGenerator)
+    {
+      return GetOrAdd(key, () => Task.FromResult(valueGenerator()));
+    }
+
+    public TValue GetOrAdd(TKey key, Func<Task<TValue>> valueGenerator)
+    {
+      TValue value;
+      if (_cache.TryGetValue<TValue>(key, out value))
+      {
+        return value;
+      }
+      value = valueGenerator().Result;
+      if (_cache.Count < Capacity)
+      {
+
+        _cache.Set<TValue>(key, value, DateTimeOffset.UtcNow.AddMinutes(60));
+
+      }
+
+      return value;
+    }
+
+    public void Remove(TKey key)
+    {
+
+      _cache.Remove(key);
+
+    }
+    public void Put(TKey key, TValue value)
+    {
+
+      if (_cache.Count < Capacity)
+      {
+        _cache.Set<TValue>(key, value, DateTimeOffset.UtcNow.AddMinutes(60));
+
+      }
+
+    }
+  }
+}
